49b71ac103ea4eae0df57de06c879eef8b0a6373
winstone-1 <winstonemuna404@gmail.com>
2026-05-04 00:42:22 +0300

    feat: add appointment scheduler, blood pressure tracker, health journal, medication reminder, and wellness tips templates

--- models.py
+++ models.py
@@ -141,3 +141,15 @@ class UserSession(db.Model):
 
     def __repr__(self):
         return f'<UserSession {self.session_token[:8]}...>'
+    
+    class MedicationReminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    med_name = db.Column(db.String(200), nullable=False)
+    dosage = db.Column(db.String(100))
+    time_of_day = db.Column(db.String(50))
+    frequency = db.Column(db.String(50))
+    instructions = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    user = db.relationship('User', backref='medications')
